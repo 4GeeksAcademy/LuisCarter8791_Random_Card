db87ca4ecd6cb7c7be37fc3fd63e96568459a5e9
Luis Carter <46801420+LuisCarter8791@users.noreply.github.com>
2024-07-16 00:01:40 +0000

    Initial commit for this project

--- src/app.js
+++ src/app.js
@@ -2,10 +2,26 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const suits = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = function(){
+  // write your code here
+
+  // '' -> suits[0]
+  // '' -> suits[1]
+  // '' -> suits[2]
+  // '' -> suits[3]
+
+  const suitIndex = Math.floor(math.random() * suits.length);
+  const randomSuit = suits[suitIndex];
+
+  const topLeftSuitDivElement = document.querySelector('.top-left-suit');
+
+  if (!topLeftSuitDivElement) {
+    console.log("No top left suit element found");
+    return;
+  }
+
+  topLeftSuitDivElement.innerText = randomSuit;
+
+}
