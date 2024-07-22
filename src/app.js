afe0c6e3458df0cb2a0e39bb026fddb474538902
Luis Carter <46801420+LuisCarter8791@users.noreply.github.com>
2024-07-22 23:49:17 +0000

    Project complete however with some errors

--- src/app.js
+++ src/app.js
@@ -1,27 +1,19 @@
-/* eslint-disable */
 import "bootstrap";
-import "./style.css";
+import "../style.css";
 
-const suits = [];
+window.onload = () => {
+  document.querySelector('.card').classList.add(generateRandomSuit());
+  document.querySelector('.card').innerHTML = generateRandomNumber();
+};
 
-window.onload = function(){
-  // write your code here
+let generateRandomNumber; () => {
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+}; 
 
-  // '' -> suits[0]
-  // '' -> suits[1]
-  // '' -> suits[2]
-  // '' -> suits[3]
-
-  const suitIndex = Math.floor(math.random() * suits.length);
-  const randomSuit = suits[suitIndex];
-
-  const topLeftSuitDivElement = document.querySelector('.top-left-suit');
-
-  if (!topLeftSuitDivElement) {
-    console.log("No top left suit element found");
-    return;
-  }
-
-  topLeftSuitDivElement.innerText = randomSuit;
-
-}
+let generateRandomSuit; () => {
+  let suit = ["diamond", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+}; 
