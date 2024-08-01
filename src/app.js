2393fb9a0dd364ea06732b64292a13e65a983ddf
Luis Carter <46801420+LuisCarter8791@users.noreply.github.com>
2024-08-01 17:55:26 +0000

    One last commit

--- src/app.js
+++ src/app.js
@@ -1,20 +1,30 @@
 let generateRandomNumber = () => {
-  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
   let indexNumbers = Math.floor(Math.random() * numbers.length);
   return numbers[indexNumbers];
-}; 
+};
 
 let generateRandomSuit = () => {
   let suit = ["diamond", "spade", "heart", "club"];
   let indexSuit = Math.floor(Math.random() * suit.length);
   return suit[indexSuit];
-}; 
-
-
-
-window.onload = () => {
-  document.querySelector('.card').classList.add(generateRandomSuit());
-  document.querySelector('.card').innerHTML = generateRandomNumber();
 };
 
-
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+};
